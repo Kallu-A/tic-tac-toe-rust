14e5cdb03ac7e49376750b70a18b74e41927e397
kallu <lucas.aries@sfr.fr>
2021-12-10 20:49:38 +0100

    format

--- src/game.rs
+++ src/game.rs
@@ -1,14 +1,14 @@
-use std::fmt::{Display, Error, Formatter};
-use termcolor::{Color, ColorChoice, ColorSpec, StandardStream, WriteColor};
 use crate::game::State::{Finish, Progress};
 use crate::get_input;
-
-
+use std::fmt::{Display, Error, Formatter};
+use termcolor::{Color, ColorChoice, ColorSpec, StandardStream, WriteColor};
 
 /// Enum Player
 #[derive(Copy, Clone, PartialEq, Debug)]
-pub enum Player { X, O }
-
+pub enum Player {
+    X,
+    O,
+}
 
 impl Player {
     /// Other change the other player and change the value to him
@@ -16,7 +16,7 @@ impl Player {
     pub fn other(mut self) -> Self {
         self = match self {
             Player::X => Player::O,
-            Player::O => Player::X
+            Player::O => Player::X,
         };
         self
     }
@@ -53,16 +53,14 @@ impl Display for Player {
 /// `None` nobody is on this hut
 /// `Player` mean someone on it
 #[derive(Copy, Clone, PartialEq, Debug)]
-struct Hut (Option<Player>);
-
+struct Hut(Option<Player>);
 
 /// Implementation de Hut
 impl Hut {
-
     /// Allow do add a player to the hut
     /// If someone already there nothing happen
     pub fn change_to(&mut self, player: Player) {
-        if let None = self.0 {
+        if self.0.is_none() {
             self.0 = Some(player);
         }
     }
@@ -70,7 +68,7 @@ impl Hut {
     pub fn assert_player(&self) -> Result<Player, Error> {
         match self.0 {
             Some(p) => Ok(p),
-            _ => { Err(Error {})}
+            _ => Err(Error {}),
         }
     }
 }
@@ -79,11 +77,13 @@ impl Hut {
 impl Display for Hut {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         match self.0 {
-            None => {write!(f, "\u{25A2}")}
-            Some(p) => {write!(f, "{}", p)}
+            None => {
+                write!(f, "\u{25A2}")
+            }
+            Some(p) => {
+                write!(f, "{}", p)
+            }
         }
-
-
     }
 }
 
@@ -92,7 +92,7 @@ enum State {
     /// `Progress` game not finish and contains the player to who is the turn
     Progress(Player),
     /// `Finish` contains option of Player mean the game is finish and if None is a draw else Player is the winner
-    Finish(Option<Player>)
+    Finish(Option<Player>),
 }
 
 impl State {
@@ -104,11 +104,8 @@ impl State {
     /// Handle the call to the player.other() on himself by match but only on progress because we don't want to change the Winner like that
     /// This functionnality make the call to other at the end of the game not a danger
     pub fn other(&mut self) {
-        match self {
-            Progress(p) => {
-                *self = Progress(p.other());
-            }
-            _ => {}
+        if let Progress(p) = self {
+            *self = Progress(p.other());
         };
     }
 }
@@ -120,126 +117,133 @@ const BOARD_LEN: usize = 3;
 /// # struct of the game tic_tac_toe
 /// Can be use easily by juste call the new function and everything is handler to make the main the more empty possible
 pub struct TicTacToe {
-
     /// Attributes which is the board of BOARD_LEN x BOARD_LEN dimension
     /// each position is a Hut wich can only be None or Player
     /// `None` significate that the hut is empty and `Player` is the player on it
     board: [[Hut; BOARD_LEN]; BOARD_LEN],
 
     /// State of the game
-    state: State
+    state: State,
 }
 
 /// Show the board on 2D on the terminal with color isn't that wonderful ?!
 impl Display for TicTacToe {
-
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         write!(f, "  A B C").unwrap();
 
-        for (index, val ) in self.board.iter().enumerate() {
-            write!(f, "\n{} ", index+1).unwrap();
+        for (index, val) in self.board.iter().enumerate() {
+            write!(f, "\n{} ", index + 1).unwrap();
             for val2 in val {
                 write!(f, "{} ", val2).unwrap();
             }
         }
         write!(f, "")
     }
-
 }
 
 impl TicTacToe {
-
     /// # Call to launch the game
     /// This function centralised the call of TicTacToe elsewhere because it's the only public function
-    pub fn new()  {
+    pub fn launch() {
         // we create the game
         let mut game = TicTacToe {
             state: State::new(Player::X),
-            board: [[ Hut(None); BOARD_LEN]; BOARD_LEN]
+            board: [[Hut(None); BOARD_LEN]; BOARD_LEN],
         };
 
         // loop who contains the logic
-        println!("Welcom to the game Tic-Tac-Toe ! ");
+        println!("Welcome to the game Tic-Tac-Toe ! ");
         loop {
             game.make_move();
             game.state.other();
-
 
             if game.calcul_if_over() {
                 println!("Game finish !");
                 if let Finish(w) = game.state {
                     match w {
-                        None => { println!("Sadly it's was a draw ! ");}
-                        Some(p) => { println!("Player : {} won fair and square", p)}
+                        None => {
+                            println!("Sadly it's was a draw ! ");
+                        }
+                        Some(p) => {
+                            println!("Player : {} won fair and square", p)
+                        }
                     }
                     println!("{}", game);
                     return;
                 }
             }
-
         }
     }
 
     /// Convert the tuple get by the stdin_handler::get_input into a tuple of `(usize,usize)`
     fn convert(tuple: (char, usize)) -> (usize, usize) {
         match tuple.0 {
-            'A' => { (0, tuple.1)}
-            'B' => { (1, tuple.1)}
-            'C' => { (2, tuple.1)}
-            _ => { panic!("Should never happen ! ") }
+            'A' => (0, tuple.1),
+            'B' => (1, tuple.1),
+            'C' => (2, tuple.1),
+            _ => {
+                panic!("Should never happen ! ")
+            }
         }
     }
 
     /// # Danger must be call if we sure that the board is not full else this will be a infinite loop
     /// ### Recursif function
-    /// while force the player to make a move
+    /// will force the player to make a move
     fn make_move(&mut self) {
         println!("{}", self);
-        match self.state {
-
-            State::Progress(player) => {
-                let tup = TicTacToe::convert(get_input(player));
-                if !self.do_move(tup, player) {
-                    let mut stdout = StandardStream::stdout(ColorChoice::Always);
-                    stdout
-                        .set_color(ColorSpec::new().set_fg(Some(Color::Red)))
-                        .expect("Error when changing color ");
-                    println!("Hut already used ! retry");
-                    stdout.reset().expect("Error when reset stdout");
-                    self.make_move()
-                }
-
+        if let State::Progress(player) = self.state {
+            let tup = TicTacToe::convert(get_input(player));
+            if !self.do_move(tup, player) {
+                let mut stdout = StandardStream::stdout(ColorChoice::Always);
+                stdout
+                    .set_color(ColorSpec::new().set_fg(Some(Color::Red)))
+                    .expect("Error when changing color ");
+                println!("Hut already used ! retry");
+                stdout.reset().expect("Error when reset stdout");
+                self.make_move()
             }
-            _ => { }
         }
     }
     /// Method who do the move in the tupple for the player if it's None
-    fn do_move(&mut self, tupple: (usize, usize), player : Player) -> bool {
+    fn do_move(&mut self, tupple: (usize, usize), player: Player) -> bool {
         let (col, line) = tupple;
-        if let None = self.board[line][col].0  {
+        if self.board[line][col].0.is_none() {
             self.board[line][col].change_to(player);
             true
-        } else { false }
+        } else {
+            false
+        }
     }
 
     /// Test if the game is_over or not
     fn calcul_if_over(&mut self) -> bool {
         // test in diagonal
-        if self.board[0][0].0 == self.board[2][2].0 && self.board[1][1].0 == self.board[2][2].0 && self.board[2][2].0 != None ||
-                    self.board[2][0].0 == self.board[0][2].0 && self.board[1][1].0 == self.board[0][2].0 && self.board[0][2].0 != None {
-
+        if self.board[0][0].0 == self.board[2][2].0
+            && self.board[1][1].0 == self.board[2][2].0
+            && self.board[2][2].0 != None
+            || self.board[2][0].0 == self.board[0][2].0
+                && self.board[1][1].0 == self.board[0][2].0
+                && self.board[0][2].0 != None
+        {
             self.state = Finish(Some(self.board[1][1].assert_player().unwrap()));
             return true;
         }
 
         for i in 0..BOARD_LEN {
             // test line
-            if self.board[i][0].0 == self.board[i][2].0 && self.board[i][1].0 == self.board[i][2].0 && self.board[i][1].0 != None {
+            if self.board[i][0].0 == self.board[i][2].0
+                && self.board[i][1].0 == self.board[i][2].0
+                && self.board[i][1].0 != None
+            {
                 self.state = Finish(Some(self.board[i][1].assert_player().unwrap()));
                 return true;
             }
             //test column
-            if self.board[0][i].0 == self.board[2][i].0 && self.board[1][i].0 == self.board[2][i].0 && self.board[0][i].0 != None {
+            if self.board[0][i].0 == self.board[2][i].0
+                && self.board[1][i].0 == self.board[2][i].0
+                && self.board[0][i].0 != None
+            {
                 self.state = Finish(Some(self.board[1][i].assert_player().unwrap()));
                 return true;
             }
--- src/stdin_handler.rs
+++ src/stdin_handler.rs
@@ -1,15 +1,12 @@
-use std::io::Write;
 use crate::game::Player;
+use std::io::Write;
 use termcolor::{Color, ColorChoice, ColorSpec, StandardStream, WriteColor};
-
 
 /// # Function to get a input of type A1 or A3
 /// use by game will loop always until a move in '[A-B-C][1-2-3]' is make
-pub fn get_input(player: Player) -> (char, usize){
-
+pub fn get_input(player: Player) -> (char, usize) {
     // create a stdout standard stream with color to better interface control
     let mut stdout = StandardStream::stdout(ColorChoice::Always);
-
 
     loop {
         // reset color at the beginning because of continue
@@ -20,7 +17,9 @@ pub fn get_input(player: Player) -> (char, usize){
         print!("Player {} enter a number : ", player);
 
         // flush to let user make their input on the same line as below
-        std::io::stdout().flush().expect("Error will flushing the terminal");
+        std::io::stdout()
+            .flush()
+            .expect("Error will flushing the terminal");
 
         // set color to red because now everything print will be error at this point
         stdout
@@ -30,38 +29,38 @@ pub fn get_input(player: Player) -> (char, usize){
         let mut buffer = String::new();
 
         if std::io::stdin().read_line(&mut buffer).is_err() {
-            println!("Can't read the line, please retry "); continue
+            println!("Can't read the line, please retry ");
+            continue;
         } else {
-
             let buffer = buffer.trim();
             if buffer.len() != 2 {
-                println!("Bad number of value ! "); continue
+                println!("Bad number of value ! ");
+                continue;
             }
 
             // collect the first value of the buffer = column and put it to uppercase to avoid difference beetween a and A
             let col = &buffer.chars().next().unwrap().to_ascii_uppercase();
             // see if is in A..=C which is all valid column
-            if ! ('A'..='C').contains(col)  {
-                println!("The column can only be A, B or C"); continue
+            if !('A'..='C').contains(col) {
+                println!("The column can only be A, B or C");
+                continue;
             }
-
 
             let line;
             // parse second value line in usize
             if let Ok(number) = buffer[1..=1].parse::<usize>() {
                 if !(1..=3).contains(&number) {
-                    println!("The line can only be 1, 2, 3"); continue
+                    println!("The line can only be 1, 2, 3");
+                    continue;
                 }
                 // here everything is good so we return
                 line = number;
                 stdout.reset().expect("Error when reset stdout");
-                return (*col, line - 1)
+                return (*col, line - 1);
             } else {
-                println!("Put a value on the line 1, 2, 3"); continue
+                println!("Put a value on the line 1, 2, 3");
+                continue;
             }
         }
-
-    };
-
-
+    }
 }
